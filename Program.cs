0be831185f917d17ca171de3ab6ca30f6954c202
ilovehleb <ilovehleb@gmail.com>
2022-08-17 18:12:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+// app.MapGet("/", () => "Hello World!");
+// app.Run();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+var app = builder.Build();
+
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(e =>
+{
+  e.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+});
+app.Run();
+
+//MVC
+
+//MODEL
+
+//VIEW
+
+//CONTROLLER
